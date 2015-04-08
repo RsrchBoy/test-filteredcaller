0f6923b49f7148c71c4e259299c6c29d9e0485a6
Chris Weyl <cweyl@alumni.drew.edu>
2015-04-08 14:56:52 -0700

    initial commit of Test::FilteredCaller

--- lib/Test/FilteredCaller.pm
+++ lib/Test/FilteredCaller.pm
@@ -1,12 +1,50 @@
 package Test::FilteredCaller;
 
-# ABSTRACT: The great new Test::FilteredCaller!
+# ABSTRACT: Find your caller in a non-Test::* namespace
 
-use Moose;
-use namespace::autoclean;
+use strict;
+use warnings;
 
+use Carp 'confess';
 
-__PACKAGE__->meta->make_immutable;
+# debugging...
+#use Smart::Comments '###';
+
+#use namespace::autoclean;
+
+use Sub::Exporter -setup => {
+    exports => [ nontest_caller => \&_build_nontest_caller ],
+    groups  => { default        => [ 'nontest_caller' ] },
+    # place-holder validation for the moment
+    #collectors => [ namespaces  => sub { 1 } ],
+};
+
+sub _build_nontest_caller {
+    my ($class, $name, $arg, $col) = @_;
+
+    ### @_
+    my @namespaces = ( qr/^Test::/ );
+    my $default_depth = 0;
+
+    return sub {
+        ### @_
+        my ($depth) = defined $_[0] ? shift : $default_depth;
+
+        ### starting: $depth
+        while (my @info = caller($depth)) {
+
+            ### @info
+            my $pkg = $info[0];
+            return @info
+                unless scalar map { $pkg =~ $_ } @namespaces;
+
+            $depth++;
+        }
+
+        confess 'Congrats!  You managed to filter ALL THE NAMESPACES.';
+    };
+}
+
 !!42;
 __END__
 
--- t/basic.t
+++ t/basic.t
@@ -2,7 +2,26 @@ use strict;
 use warnings;
 
 use Test::More;
+use Test::FilteredCaller;
 
-fail 'No tests!';
+# test with defaults
+my @info = nontest_caller;
+note explain \@info;
+is $info[0], 'main'      => 'package correct';
+is $info[1], 't/basic.t' => 'filename correct';
+is $info[2], __LINE__-4  => 'line correct';
+
+{
+    package Test::Frobnip;
+
+    use Test::FilteredCaller;
+    sub bar { nontest_caller }
+}
+
+@info = Test::Frobnip::bar;
+note explain \@info;
+is $info[0], 'main'      => 'package correct';
+is $info[1], 't/basic.t' => 'filename correct';
+is $info[2], __LINE__-4  => 'line correct';
 
 done_testing;
