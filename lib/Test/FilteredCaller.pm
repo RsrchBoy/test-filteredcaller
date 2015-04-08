9e0ddd734b7c22471552d00d0bb24e055c62e561
Chris Weyl <cweyl@alumni.drew.edu>
2015-04-08 14:19:12 -0700

    initial commit

---
+++ lib/Test/FilteredCaller.pm
@@ -0,0 +1,19 @@
+package Test::FilteredCaller;
+
+# ABSTRACT: The great new Test::FilteredCaller!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
